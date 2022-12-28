93adbe4766f2deac2b8bf364cfa7a772f8a290a3
alexiapop <alexia.pop90@yahoo.com>
2022-12-28 20:57:49 +0200

    added new post screen , removed default action bar , used customized toolbar where needed

---
+++ app/src/main/java/com/example/project/NewPostFragment.kt
@@ -0,0 +1,29 @@
+package com.example.project
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.project.databinding.FragmentNewPostBinding
+
+class NewPostFragment: Fragment() {
+
+    private lateinit var binding: FragmentNewPostBinding
+    private val viewModel: SharedViewModel by viewModels()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentNewPostBinding.inflate(inflater, container, false)
+        binding.closeButton.setOnClickListener{
+            findNavController().navigateUp()
+        }
+        return binding.root
+    }
+}
